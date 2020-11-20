c7e93e6f7e4c6984ee8e1e97c2c387a17ccf9347
HarryNdegwa <harrisonndegwa65@gmail.com>
2020-11-20 23:40:31 +0300

    Created a simple path

--- Path1.java
+++ Path1.java
@@ -1,6 +1,14 @@
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+
 public class Path1{
 
     public static void main(String[] args){
-        
+
+        Path path = Paths.get(".");
+
+        System.out.println("The current direcorty path is "+path.toAbsolutePath());
+
     }
 }
