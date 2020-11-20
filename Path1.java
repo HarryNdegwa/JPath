3ea52bc6a1ef1d6b7c561558136ff8cee735f073
HarryNdegwa <harrisonndegwa65@gmail.com>
2020-11-20 23:56:05 +0300

    Tested path methods

--- Path1.java
+++ Path1.java
@@ -6,9 +6,13 @@ public class Path1{
 
     public static void main(String[] args){
 
-        Path path = Paths.get(".");
+        Path path = Paths.get("\\test.txt");
 
         System.out.println("The current direcorty path is "+path.toAbsolutePath());
+
+        System.out.println("File name "+path.getFileName());
+
+        System.out.println("Path components count "+path.toAbsolutePath().getNameCount());
 
     }
 }
